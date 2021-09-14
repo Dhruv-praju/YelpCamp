3ac1a3e118e0fea37a5cc2ef75f23713369a8f69
dhruv <prajapati.dp@somaiya.edu>
2021-09-14 13:06:13 +0530

    added flash message

--- app.js
+++ app.js
@@ -3,11 +3,24 @@ const express = require('express')
 const app = express()
 const {render} = require('ejs')
 const methodOverride = require('method-override')
+const session = require('express-session')
+const flash = require('connect-flash')
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
 app.use(methodOverride('_method'))
 
+const sessionOptions = {secret:'mylittlesecret', resave:false, saveUninitialized:false}
+app.use(session(sessionOptions))
+app.use(flash())
+
+app.use((req, res, next)=>{
+    res.locals.create_success = req.flash('create_success')   //make success key variable to access message
+    res.locals.update_success = req.flash('update_success')
+    res.locals.delete_success = req.flash('delete_success')
+    res.locals.error = req.flash('error')
+    next()
+})
 /** Database setup */
 
 const mongoose = require('mongoose')
@@ -39,6 +52,7 @@ app.post('/campgrounds', async (req, res)=>{
     // let new_campgrd = { ...req.body , country:'USA'}
     const {name, image, price, description, ...location} = req.body
     const added_campgrd = await Campground.create({name, image, price, description,location})
+    req.flash('create_success','Sucessfully created new Campground')
     res.redirect('campgrounds')
 })
 app.get('/campgrounds/new', (req, res)=>{
@@ -59,11 +73,13 @@ app.put('/campgrounds/:id', async (req, res)=>{
     const {id} = req.params
     // console.log(req.body);
     const upd_campground = await Campground.findByIdAndUpdate(id, req.body, {runValidators:true, new:true})
+    req.flash('update_success', 'Sucessfully updated the Campground')
     res.redirect(`/campgrounds/${upd_campground._id}`)
 })
 app.delete('/campgrounds/:id', async(req, res)=>{
     const {id} = req.params
     const campground = await Campground.findByIdAndDelete(id)
+    req.flash('delete_success', 'Sucessfully deleted the Campground')
     res.redirect('/campgrounds')
 })
 
