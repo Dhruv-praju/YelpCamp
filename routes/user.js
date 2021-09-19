adc2067cd39f5097415127b51d1138c742c92ced
dhruv <prajapati.dp@somaiya.edu>
2021-09-19 17:16:40 +0530

    made user register form and transfer flash on boilerplate

--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,24 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
+const catchAsync = require('../utils/catchAsync')
 
 router.get('/register',(req, res)=>{
     res.render('users/register.ejs')
 })
-router.post('/register',async (req, res)=>{
-    const {email, username, password} = req.body
-    // make a new user instance
-    const new_usr = new User({email, username})
-    const registered_usr = await User.register(new_usr, password)
-    console.log(registered_usr);
-    // req.flash( )
-    res.redirect('/')
-})
+router.post('/register',catchAsync( async (req, res)=>{
+    try {
+        const {email, username, password} = req.body
+        // make a new user instance
+        const new_usr = new User({email, username})
+        const registered_usr = await User.register(new_usr, password)   // this will make salt and passwd for new user
+        console.log(registered_usr);
+        req.flash('success', 'Welcome to Yelp Camp!')
+        res.redirect('/campgrounds')
+    } catch (e) {
+        req.flash('error', e.message)
+        res.redirect('/register')
+    }
+}))
 
 module.exports = router
